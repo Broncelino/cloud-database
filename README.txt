5c73679bac8a0410b923ea1816aaf217b6bf07d0
Broncelino <bsojn27@gmail.com>
2022-10-12 02:22:33 -0600

    fixed the program to a working start

---
+++ README.txt
@@ -0,0 +1,47 @@
+# Overview
+
+My name is Bryson Williams I have always loved computers and have been programming for a few years now.
+I've already made other programs in various languages and have them 
+posted to this github. As of now 9/17/22 I am aiming to become a Database admin or something else in that field.
+
+{Provide a description the software that you wrote and how it integrates with a Cloud Database.  Describe how to use your program.}
+
+I wrote a program in javascript to interface with the google firebase database. I took the various keys and Id's
+given for the database and used the firebase imports to connect my webpage to the database giving it access to read, modify and delete it.
+
+
+{Describe your purpose for writing this software.}
+I like databases and am making one for another website i made but the data entry aspect of firebase was poor.
+If you could set static fields for every new document you made in a collection then I couldn't figure out how.
+With this program instead of retyping the name of each field and setting the data type you can just tab through
+typing all the data much faster that if you were to use the built in firebase data entry.
+
+https://youtu.be/jE6_LdSBU3M
+
+# Cloud Database
+Firebase is a google owned, non-relational cloud database.
+It was designed to be easily programable so you can customize the program around it however you like.
+
+{Describe the structure of the database that you created.}
+The database consists of a single table or collection titled 'countries' with documents that get added to it.
+The documents would share the name of the country they represent.
+In each of the documents the data held is data about the country they represent.
+
+# Development Environment
+
+{Describe the tools that you used to develop the software}
+
+{Describe the programming language that you used and any libraries.}
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+* [Web Site Name](http://url.link.goes.here)
+* [Web Site Name](http://url.link.goes.here)
+
+# Future Work
+
+{Make a list of things that you need to fix, improve, and add in the future.}
+* set up authorization requirements
+* store the apikey and others securely with the webhost.
+* Item 3
--- base.js
+++ base.js
@@ -1,11 +1,12 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-//import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import { getFirestore, doc, getDocs, collection, deleteDoc,
+ setDoc } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
 
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const firebaseConfig = { //database constants; apikey, authdomain, projectId, appId, etc
   apiKey: "AIzaSyCFHq56SEDG1bRcgcukM3e-r4ALKPbeVBE",
   authDomain: "test-9c720.firebaseapp.com",
   projectId: "test-9c720",
@@ -19,11 +20,51 @@ initializeApp(firebaseConfig);
 
 const db = getFirestore()
 
-const ref = collection(db, 'Questions')
+const ref = collection(db, 'countries')
 
-console.log(ref);
+console.log(getDocs(ref));
 
 getDocs(ref)
     .then((snapshot) => {
-        console.log(snapshot.docs);
+        let countries = [];
+        snapshot.docs.forEach((doc) => {
+          countries.push({ ...doc.data()})
+        })
+        console.log(countries) //displays documents when the page was loaded
     })
+
+
+document.getElementById("add").addEventListener('click', (e) => {
+  let name = document.getElementById("doc").value
+  let data = { name: name, field1: document.getElementById("field1").value, field2: document.getElementById("field2").value}
+  let docum = doc(db, 'countries', name)
+  setDoc(docum, data)
+  .then(() => {
+    console.log("Document has been added/modified successfully");
+  })
+  .catch(error => {
+    console.log(error);
+  })
+})
+document.getElementById("del").addEventListener('click', (e) => {
+  let name = document.getElementById("deldoc").value
+  let docum = doc(db, 'countries', name)
+  deleteDoc(docum)
+  .then(() => {
+    console.log("Document has been deleted successfully");
+  })
+  .catch(error => {
+    console.log(error);
+  })
+})
+document.getElementById("check").addEventListener('click', (e) => {
+    getDocs(ref)
+    .then((snapshot) => {
+        let countries = [];
+        snapshot.docs.forEach((doc) => {
+          countries.push({ ...doc.data()})
+        })
+        console.log(countries)
+    })
+  
+})
